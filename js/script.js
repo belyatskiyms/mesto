d3f63d347cee20119c29c97ec82755cdaf423d06
Mikhail Belyatskiy <belyatskiyms@gmail.com>
2021-04-17 04:08:59 +0300

    fix: Исправление ошибок (1 ревью)

--- js/script.js
+++ js/script.js
@@ -3,23 +3,25 @@ let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileVocation = document.querySelector('.profile__vocation');
-let popupButton = document.querySelector('.popup__form');
+let popupForm = document.querySelector('.popup__form');
 let inputName = document.querySelector('#name');
 let inputVocation = document.querySelector('#vocation');
-
-editButton.addEventListener('click', () => {
-    popup.classList.add('popup_opened')
-    inputName.value = profileName.innerText;
-    inputVocation.value = profileVocation.innerText;     
-});
-popupCloseButton.addEventListener('click', () => popup.classList.remove('popup_opened') );
-popupButton.addEventListener('submit', formSubmitHandler); 
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileVocation.textContent = inputVocation.value;
+    popupClose();
+}
+function popupOpen(){
+    popup.classList.add('popup_opened');
+    inputName.value = profileName.textContent;
+    inputVocation.value = profileVocation.textContent;
+}
+function popupClose(){
     popup.classList.remove('popup_opened');
 }
 
-
+editButton.addEventListener('click', popupOpen);  //Открывает popup
+popupCloseButton.addEventListener('click', popupClose); //Закрывает popup
+popupForm.addEventListener('submit', formSubmitHandler); //Обрабатывает данные формы
